f2103e619a421cb53b9e12dd976248e8f3659ec8
Jyothig26 <104355449+Jyothig26@users.noreply.github.com>
2022-10-18 11:53:38 +0530

    folder

---
+++ Test/Login.py
@@ -0,0 +1,21 @@
+from homepage import Homepage
+
+from time import sleep
+
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+with sync_playwright () as p:
+ browser = p.chromium.launch(headless=False)
+ page = browser.new_page()
+ page.goto('https://hrmsdev1.azurewebsites.net/')
+ home_page = Homepage(page)
+ sleep(5)
+ home_page.Username.click()
+ home_page.Username.fill("dotnethod@intonenetworks.com")
+ sleep(10)
+ home_page.password.fill("Password1!")
+ sleep(15)
+ home_page.login.click()
+ sleep(20)
+
+ browser.close()
